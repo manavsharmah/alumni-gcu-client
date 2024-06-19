f81db8f5f217628923b9bbdb9d6dde6d13a5ef02
Nishant <150470870+nishanth2256@users.noreply.github.com>
2024-06-19 14:57:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import "./styles/App.css";
-import Header from './components/common/Header';
-import Footer from './components/common/Footer'
 import Home from './pages/Home';
- second
 import RootLayout from './components/RootLayout';
 import Admindashboard from './pages/Admindashboard';
 import Login from './components/common/Login';
@@ -15,16 +11,9 @@ import ResetPassword from './components/common/ResetPassword';
 
 
 
-import  Login  from "./components/common/Login";
-import Register from './components/common/Register';
-import ResetPassword from './components/common/ResetPassword';
-
- main
-
 
 function App() {
   return (
- second
     <Router>
         <div>
           <Routes>
@@ -44,24 +33,4 @@ function App() {
 
 
 export default App;
-    <React.Fragment>
-      <header>
-        <Header />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/reset-password' element={<ResetPassword />} />
-        </Routes>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </React.Fragment>
-  );
-}
 
-export default App;
- main

6203662e60f0d8ded9d57e1aba46b4e6639252df
Nishant <150470870+nishanth2256@users.noreply.github.com>
2024-06-19 14:58:55 +0530

    Update Welcome.js

--- src/pages/specific/Welcome.js
+++ src/pages/specific/Welcome.js
@@ -1,6 +1,5 @@
 // import axios from "axios";
 // import React, { useEffect, useState } from "react";
- second
 // axios.defaults.withCredentials = true;
 
 // const Welcome = () => {
@@ -50,40 +49,3 @@
 // };
 
 // export default Welcome;
-
-// axios.defaults.withCredentials = true
-
-// const Welcome = () => {
-//     const [user, setUser] = useState();
-    
-//     const sendRequest = async() => {
-//         const res = await axios.get('http://localhost:5000/api/user', {
-//             withCredentials: true
-//         }) 
-//         .catch((err) => console.log(err)); 
-//         const data = await res.data;
-//         console.log(data);
-//         return data;     
-//     }
-    
-//     useEffect(() => {
-//         sendRequest().then((data) => setUser(data.user));
-//     },[])    
-
-//     return(
-//         <div> 
-//             <h1>Welcome</h1>
-//             <h1>Welcome</h1>
-//             <h1>Welcome</h1>       
-//             {user && <h1>{user.name}</h1>}
-//             {user && <h1>{user.name}</h1>} 
-//             {user && <h1>{user.name}</h1>} 
-//             {user && <h1>{user.name}</h1>} 
-//             {user && <h1>{user.name}</h1>}  
-//         </div>
-//     );
-    
-// }
-
-// export default Welcome;
- main

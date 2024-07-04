f35557d83096e7b67beec9f8d9aadc220a56713d
JyotirmoyBarman <135860979+JyotirmoyBarman@users.noreply.github.com>
2024-07-04 12:17:25 +0530

    Added detail folder for all the sub pages

---
+++ src/pages/detail/Alumnus.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Alumnus = () => {
+    return (
+        <div className='container d-flex align-items-center justify-content-center min-vh-20'>
+            <div>
+                <h2 className='text-center'>Alumnus-Stake Holder Forum</h2>
+                <div className="row justify-content-center mt-5">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card Title</h5>
+                                <p className="card-text">
+                                    This is some text within a card body. You can use this space to provide an overview of the content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Alumnus;
---
+++ src/pages/detail/Vision.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Vision = () => {
+    return (
+        <div className='container main-header'>
+            <h2 className='text-center'>Vision</h2>
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Card Title</h5>
+                            <p className="card-text">
+                                This is some text within a card body. You can use this space to provide an overview of the content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Vision;

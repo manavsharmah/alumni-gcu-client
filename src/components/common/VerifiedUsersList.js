3ed866aadc2dd80869dd0782f3b6021826f78f50
Nishant <150470870+nishanth2256@users.noreply.github.com>
2024-07-18 19:56:24 +0530

    Update VerifiedUsersList.js

--- src/components/common/VerifiedUsersList.js
+++ src/components/common/VerifiedUsersList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axiosInstance from '../../services/api';
+import api from '../../services/api';
 import "../components.css";
 
 const VerifiedUsersList = () => {

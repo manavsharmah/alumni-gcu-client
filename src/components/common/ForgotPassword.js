4bdfc1c29719577c3e5f5685e938cb4a1f99ef9e
Nishant <150470870+nishanth2256@users.noreply.github.com>
2024-07-19 14:35:10 +0530

    Update ForgotPassword.js

--- src/components/common/ForgotPassword.js
+++ src/components/common/ForgotPassword.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "./components.css";
+import "../components.css";
 import { useNavigate } from 'react-router-dom';
 import api from '../../services/api';
 

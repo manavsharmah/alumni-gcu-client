0d1d3d07a818748330d763c7a939cab9b85a1a96
Fahim Mashud Barbhuiya <fahimmashud@gmail.com>
2024-08-25 20:21:56 +0530

    Update Welcome.js
    Added a new div class for posting messages on the feed page

--- src/pages/specific/Welcome.js
+++ src/pages/specific/Welcome.js
@@ -1,14 +1,42 @@
 import axios from "axios";
 import VerifiedUsersList from "../../components/common/VerifiedUsersList";
+import { useState } from "react";
 axios.defaults.withCredentials = true;
 
 const Welcome = () => {   
+    const [postContent, setPostContent] = useState("");
+
+    const handlePostChange = (e) => {
+        setPostContent(e.target.value);
+    };
+
+    const handlePostSubmit = () => {
+        console.log("Posting:", postContent);
+        setPostContent("");
+    };
+
     return (
-        <div className="users-container">
-            <VerifiedUsersList />
+        <div className="page-container">
+            <div className="content-wrapper">
+                <div className="post-section">
+                    <div className="post-form">
+                        <div className="profile-picture-small"></div>
+                        <input 
+                            type="text" 
+                            className="post-input" 
+                            placeholder="What's on your mind?"
+                            value={postContent}
+                            onChange={handlePostChange}
+                        />
+                        <button className="post-button" onClick={handlePostSubmit}>Post</button>
+                    </div>
+                </div>
+                <div className="users-container">
+                    <VerifiedUsersList />
+                </div>
+            </div>
         </div>
     )
-    
 }
 
 export default Welcome;

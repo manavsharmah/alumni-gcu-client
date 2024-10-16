3532b0404de15702bbb951a29eb7a0c2c77f07a4
Nishant <150470870+nishanth2256@users.noreply.github.com>
2024-10-16 19:34:15 +0530

    Update RootLayout.js

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -5,8 +5,8 @@ import Bottombar from './common/Bottombar';
 
 const RootLayout = () => {
   return (
-    <div className="d-flex flex-column min-vh-100 ">
-      <Topbar />
+    <div className="d-flex flex-column min-vh-100 ">  
+      <Topbar /> // Shows the topbar at the top of the page
 
       <section className="flex flex-1 h-full">
         <Outlet />

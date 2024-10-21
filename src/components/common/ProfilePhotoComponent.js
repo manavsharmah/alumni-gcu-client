9d054e6c3e11539a869ba992e3ed7c8b2d233b54
PriyanujBora <priybora01@outlook.com>
2024-10-21 23:05:24 +0530

    made a new component to fetch profile picture of the users based on their id

---
+++ src/components/common/ProfilePhotoComponent.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+const ProfilePhoto = ({ userId, className = '' }) => {
+  const [profilePhoto, setProfilePhoto] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProfilePhoto = async () => {
+      if (!userId) return;
+      try {
+        // console.log('Fetching profile photo for userId:', userId); 
+        const response = await api.get(`/user/profile-photo/${userId}`);
+        setProfilePhoto(response.data?.profilePhoto || null);
+      } catch (error) {
+        if (error.name !== 'AbortError') {
+          console.error('Error fetching profile photo:', error);
+          setProfilePhoto(null);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProfilePhoto();
+  }, [userId]);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <img
+      src={profilePhoto ? `http://localhost:5000/${profilePhoto.replace(/\\/g, "/")}` : `../assets/profile-placeholder.svg`}
+      alt="Profile"
+      className={`profile-photo ${className}`}
+    />
+  );
+};
+
+export default ProfilePhoto;

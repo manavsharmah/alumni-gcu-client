77a9631444f9bf5c1cddc1cdcdf1f3a15af73c14
JyotirmoyBarman <135860979+JyotirmoyBarman@users.noreply.github.com>
2024-10-29 15:18:48 +0530

    Update Contact.js

--- src/pages/detail/Contact.js
+++ src/pages/detail/Contact.js
@@ -1,23 +1,23 @@
-import React from 'react';
+// import React from 'react';
 
 
-const Chapters = () => {
-    return (
-        <div className='container main-header'>
-            <div className="content-wrapper">
-                <div className="content">
-                    <h2 className='text-center'>Contact</h2>
-                    <div className="card">
-                        <div className="card-body">
-                            <p className="card-text">
-                                This is some text within a card body. You can use this space to provide an overview of the content.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
+// const Chapters = () => {
+//     return (
+//         <div className='container main-header'>
+//             <div className="content-wrapper">
+//                 <div className="content">
+//                     <h2 className='text-center'>Contact</h2>
+//                     <div className="card">
+//                         <div className="card-body">
+//                             <p className="card-text">
+//                                 This is some text within a card body. You can use this space to provide an overview of the content.
+//                             </p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Chapters;
+// export default Chapters;

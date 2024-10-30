b5ef34ca95405ba4c56fdeb039455bea74dc3fe8
Fahim Mashud Barbhuiya <fahimmashud@gmail.com>
2024-10-31 01:21:37 +0530

    Added public routes that redirect logged in users

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import AdminLogin from "./components/common/AdminLogin";
 import ForgotPassword from "./components/forms/ForgotPassword";
 import RootLayout from "./components/RootLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { PublicRoute } from "./components/PublicRoute";
 import AdminLayout from "./pages/admin/AdminLayout";
 
 // Pages
@@ -52,115 +53,113 @@ import {
 } from "./pages/detail/About";
 
 function App() {
-	return (
-		<UserProvider>
-			<React.Fragment>
-				<div className="w-full md:flex">
-					<section className="flex flex-1 h-full">
-						<Routes>
-							<Route element={<RootLayout />}>
-								{/* Open Routes */}
-								<Route path="/admin-login" element={<AdminLogin />} />
-								<Route path="/login" element={<Login />} />
-								<Route path="/register" element={<Register />} />
-								<Route path="/" element={<Home />} />
-								<Route path="/overview" element={<Overview />} />
-								<Route path="/vision" element={<VisionAndMission />} />
-								<Route path="/objectives" element={<Objectives />} />
-								<Route path="/council" element={<GoverningCouncil />} />
-								<Route path="/alumnus" element={<Alumnus />} />
-								<Route path="/top-alumni" element={<TopAlumni />} />
-								<Route path="/news" element={<NewsList />} />
-								<Route path="/news/:id" element={<SingleNews />} />
-								<Route path="/gallery" element={<Gallery />} />
-								<Route path="/album/:id" element={<SingleAlbum />} />
-								<Route path="/contactus" element={<ContactUs />} />
-								<Route path="/feedback" element={<FeedbackForm />} />
-								<Route path="/scholarship" element={<Scholarship />} />
-								<Route path="/events" element={<Events />} />
-								<Route path="/events/:id" element={<SingleEvent />} />
-								<Route path="/copyright" element={<Copyright />} />
-								<Route path="/disclaimer" element={<Disclaimer />} />
-								<Route path="/termsofuse" element={<TermsOfUse />} />
-								<Route path="/privacypolicy" element={<PrivacyPolicy />} />
-								<Route path="/forgot-password" element={<ForgotPassword />} />
-								<Route path="/donations" element={<Donations />} />
+  return (
+    <UserProvider>
+      <React.Fragment>
+        <div className="w-full md:flex">
+          <section className="flex flex-1 h-full">
+            <Routes>
+              <Route element={<RootLayout />}>
+                {/* Public Routes that redirect logged-in users */}
+                <Route path="/login" element={<PublicRoute element={<Login />} />} />
+                <Route path="/register" element={<PublicRoute element={<Register />} />} />
+                <Route path="/forgot-password" element={<PublicRoute element={<ForgotPassword />} />} />
+                <Route path="/admin-login" element={<PublicRoute element={<AdminLogin />} />} />
 
-								{/* Admin + User Routes */}
-								<Route
-									path="/welcome"
-									element={
-										<ProtectedRoute
-											element={<FeedHome />}
-											requiredRole={["admin", "user"]}
-										/>
-									}
-								/>
-								<Route
-									path="/reset-password"
-									element={
-										<ProtectedRoute
-											element={<ResetPassword />}
-											requiredRole={["admin", "user"]}
-										/>
-									}
-								/>
-								<Route
-									path="/profile"
-									element={
-										<ProtectedRoute element={<Profile />} requiredRole={["admin", "user"]} />
-									}
-								/>
+                {/* Open Routes */}
+                <Route path="/" element={<Home />} />
+                <Route path="/overview" element={<Overview />} />
+                <Route path="/vision" element={<VisionAndMission />} />
+                <Route path="/objectives" element={<Objectives />} />
+                <Route path="/council" element={<GoverningCouncil />} />
+                <Route path="/alumnus" element={<Alumnus />} />
+                <Route path="/top-alumni" element={<TopAlumni />} />
+                <Route path="/news" element={<NewsList />} />
+                <Route path="/news/:id" element={<SingleNews />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/album/:id" element={<SingleAlbum />} />
+                <Route path="/contactus" element={<ContactUs />} />
+                <Route path="/feedback" element={<FeedbackForm />} />
+                <Route path="/scholarship" element={<Scholarship />} />
+                <Route path="/events" element={<Events />} />
+                <Route path="/events/:id" element={<SingleEvent />} />
+                <Route path="/copyright" element={<Copyright />} />
+                <Route path="/disclaimer" element={<Disclaimer />} />
+                <Route path="/termsofuse" element={<TermsOfUse />} />
+                <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+                <Route path="/donations" element={<Donations />} />
 
-								<Route
-									path="/profile/:id"
-									element={
-										<ProtectedRoute element={<Profile />} requiredRole={["admin", "user"]} />
-									}
-								/>
-								
-								<Route
-									path="/change-profile-picture"
-									element={
-										<ProtectedRoute element={<ChangeProfilePicture />} requiredRole={["admin", "user"]} />
-									}
-								/>
+                {/* Admin + User Routes */}
+                <Route
+                  path="/welcome"
+                  element={
+                    <ProtectedRoute
+                      element={<FeedHome />}
+                      requiredRole={["admin", "user"]}
+                    />
+                  }
+                />
+                <Route
+                  path="/reset-password"
+                  element={
+                    <ProtectedRoute
+                      element={<ResetPassword />}
+                      requiredRole={["admin", "user"]}
+                    />
+                  }
+                />
+                <Route
+                  path="/profile"
+                  element={
+                    <ProtectedRoute element={<Profile />} requiredRole={["admin", "user"]} />
+                  }
+                />
+                <Route
+                  path="/profile/:id"
+                  element={
+                    <ProtectedRoute element={<Profile />} requiredRole={["admin", "user"]} />
+                  }
+                />
+                <Route
+                  path="/change-profile-picture"
+                  element={
+                    <ProtectedRoute element={<ChangeProfilePicture />} requiredRole={["admin", "user"]} />
+                  }
+                />
+                <Route
+                  path="/update-profile"
+                  element={
+                    <ProtectedRoute
+                      element={<UpdateProfile />}
+                      requiredRole={["admin", "user"]}
+                    />
+                  }
+                />
+              </Route>
 
-								<Route
-									path="/update-profile"
-									element={
-										<ProtectedRoute
-											element={<UpdateProfile />}
-											requiredRole={["admin", "user"]}
-										/>
-									}
-								/>
-							</Route>
-
-							{/*  Admin Routes  */}
-							<Route
-								element={
-									<ProtectedRoute
-										element={<AdminLayout />}
-										requiredRole="admin"
-									/>
-								}
-							>
-								<Route path="/admin-dashboard" element={<AdminDashboard />} />
-								<Route path="/alumni-archive" element={<AlumniArchive />} />
-								<Route path="/news-form" element={<AdminNewsForm />} />
-								<Route path="/events-form" element={<AdminEventsForm />} />
-								<Route path="/photo-upload-form" element={<PhotoUpload />} />
-								<Route path="/email-form" element={<AdminEmailForm />} />
-								<Route path="/add-bulk-alumni" element={<BulkAddAlumni />} />
-							</Route>
-
-						</Routes>
-					</section>
-				</div>
-			</React.Fragment>
-		</UserProvider>
-	);
+              {/* Admin Routes */}
+              <Route
+                element={
+                  <ProtectedRoute
+                    element={<AdminLayout />}
+                    requiredRole="admin"
+                  />
+                }
+              >
+                <Route path="/admin-dashboard" element={<AdminDashboard />} />
+                <Route path="/alumni-archive" element={<AlumniArchive />} />
+                <Route path="/news-form" element={<AdminNewsForm />} />
+                <Route path="/events-form" element={<AdminEventsForm />} />
+                <Route path="/photo-upload-form" element={<PhotoUpload />} />
+                <Route path="/email-form" element={<AdminEmailForm />} />
+                <Route path="/add-bulk-alumni" element={<BulkAddAlumni />} />
+              </Route>
+            </Routes>
+          </section>
+        </div>
+      </React.Fragment>
+    </UserProvider>
+  );
 }
 
-export default App;
+export default App;

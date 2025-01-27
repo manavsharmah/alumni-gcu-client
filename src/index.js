987f0baa537e79cfa244bd979166ecb4efd5c9c8
PriyanujBora <priybora01@outlook.com>
2025-01-27 21:17:37 +0530

    fix: scroll to top on route change
    - used ScrollToTop component to scroll to top on route change
    - added ScrollToTop component to index.js and wrapped App component
    - removed ScrollToTop useffect used explicitly in FeedPostView.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>        
-        <App />     
+    <BrowserRouter>
+        <ScrollToTop />        
+            <App />     
     </BrowserRouter>
 )

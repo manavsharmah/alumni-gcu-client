9eca9e5629ff1b7e1b819b6e1ef5b4c3d1d75077
manavsharmah <manavsarmadk@gmail.com>
2025-03-03 21:05:54 +0530

    Jai shree Ram

--- src/components/common/AlumniAchieverCard.js
+++ src/components/common/AlumniAchieverCard.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import '../components.css'; // Assuming you have a separate CSS file for styling
 
-const AlumniCard = ({ image, name, department, designation, company }) => {
+const AlumniCard = ({ image, name, batch, curr_pos, curr_emp, message }) => {
   return (
     <div className="alumni-card">
       <div className="alumni-image">
         <img src={image} alt={name} />
         <div className="alumni-name">
           <h4>{name}</h4>
-          <p>{department}</p>
+          <p>{batch}</p>
         </div>
       </div>
       <div className="alumni-info">
-        <p>{designation}</p>
-        <p>{company}</p>
+        <p>{curr_pos}</p>
+        <p>{curr_emp}</p>
+        <p>{message}</p>
       </div>
     </div>
   );
--- src/pages/articles/AlumniAchievers.js
+++ src/pages/articles/AlumniAchievers.js
@@ -11,9 +11,7 @@ const AlumniAchievers = () => {
         </div>
         <div className="goal-content-container">
             <div className="goal-content">
-                <div className="goal-sub-heading">
-                    <h2>Top Alumni in the Lime Light</h2>
-                </div>
+                
                 <div className="article-text">
                     <p>
                         A University's alumni are the reflection of its past, a representation of its present, and a link to 
@@ -28,46 +26,64 @@ const AlumniAchievers = () => {
                         and are occupied internationally. The GCUAA has an Alumni Office on the 3rd Floor of the 
                         Admin Building at GCU. The GCUAA has recently contributed Rs. 2 lacs from the association 
                         fund for the University’s development purposes.
-                    </p>
+                    </p> <br />
+                    <div className="goal-sub-heading">
+                        <h2>Top Alumni in the Lime Light</h2>
+                    </div>
+                    <ol>
+                        <li>Mr. Sanjay Sinha, Senior Memory Design Engineer at Intel Corporation. Mr. Sinha is an alumnus of the Department of ECE at GIMT, graduating in the 2012-2016 batch.</li>
+                        
+                        <li>Ms Smita Paul, Design Verification Engineer at Aionsi India Pvt Ltd in Bangalore. An esteemed alumnus of the ECE Department at GIMT (2015-2019)</li>
+                        
+                        <li>Ms. Amita Sengupta, Technical Lead, VLSI Verification Engineer, Capgemini Engineering Hyderabad. Alumnus of ECE department of GIMT (2007-2011) batch</li>
+                        
+                        <li>Reemee Barman, Superintendent of State Tax, Finance Department, Government of Assam. She is an alumnus of the EE department of the GIMT 2012-16 batch</li>
+                        
+                        <li>Biplob Jyoti Saikia, Sub Divisional Engineer, APDCL, Nagaon Electrical Subdivision. He is an ex-employee in the department of EE and also an alumnus of the same department of GIMT 2009-2013 batch</li>
+                        
+                        <li>Gaurav Sharma is currently working in Federal Bank Limited as an Associate. He pursued a B.Tech in Mechanical Engineering from Girijananda Choudhury Institute of Management and Technology, Azara 2018-22 batch. He is also pursuing an MBA in Financial Management from GLA University in online mode.</li>
+                        
+                        <li>Manash Kalita, currently working as a JE in the Public Health Engineering department. He is an esteemed alumnus of the Department of Civil Engineering of GIMT, 2019-23 batch.</li>
+                        
+                        <li>Arundhati Devi is currently working as a JE in the Public Health Engineering department. She is an esteemed alumnus of the GIMT Civil Engineering department, 2018-22 batch.</li>
+                        
+                        <li>Sucharita Ghosh, Design Verification Engineer at Tech Mahindra Cerium Systems Pvt Ltd. She is an alumnus of the ECE department of GIMT 2015-19 batch.</li>
+                        
+                        <li>Amit Mundra, currently working in Zepto Staff as a Software Engineer. He an alumnus of the department of CSE 2013-17 batch of GIMT</li>
+                        
+                        <li>Tina Sarma, Graduate Engineer Trainee at Kirloskar Oil Engines. She is an alumnus of the department of Mechanical Engineering, 2018-22 batch</li>
+                    </ol>
 
                 </div>
                 <div className="alumni-card-container">
+                    <div className="goal-sub-heading">
+                        <h2>Alumni Message :</h2>
+                    </div> <br />
                     <AlumniCard 
-                        image="./assets/gcuregistrar.jpg" 
-                        name="Alumni Name" 
-                        department="Physics Department" 
-                        designation="Designation" 
-                        company="Company" 
+                        image="../../assets/Hirakjyoti_Goswami.jpg"
+                        name="Mr. Hirakjyoti Goswami" 
+                        batch="Batch : 2011-2015" 
+                        curr_pos="Current position: Executive Officer, Department of Production." 
+                        curr_emp="Current Employer: Hetero Healthcare limited (Unit II), Palasbari."
+                        message=" Message: As a proud alumnus of Girijananda Chowdhury University, I am honoured to reconnect with the Department of Pharmaceutical Sciences, which played a pivotal role in shaping my career. The dedication of the faculty and the enriching academic environment provided me with a strong foundation in pharmaceutical sciences. To the current students, make the most of your time here, embrace challenges, and aim for excellence. I am always happy to connect and contribute to our vibrant alumni network. Wishing the Department of Pharmaceutical Sciences and Girijananda Chowdhury University continued success and growth." 
                     />
-                    <AlumniCard
-                        image="./assets/gcuregistrar.jpg"
-                        name="Alumni Name"
-                        department="Physics Department"
-                        designation="Designation"
-                        company="Company"
+                    <AlumniCard 
+                        image="../../assets/Ankita_Kashyap.jpg" 
+                        name="Dr. Ankita Kashyap " 
+                        batch="Batch: 2008-2013" 
+                        curr_pos="Current Position: Lecturer " 
+                        curr_emp="Current Employer: Institute of pharmacy, Assam Medical college; Dibrugarh, Assam"
+                        message=" Message: Greetings from a proud alumnus of the Department of Pharmaceutical Sciences, Girijananda Chowdhury University! My time at GCU laid a strong foundation for my career, thanks to the exceptional faculty and learning environment. To current students, make the most of these opportunities and aim high. Let’s stay connected and support our alma mater's growth. Wishing continued success to the department of Pharmaceutical Sciences and Girijananda Chowdhury University ." 
                     />
-                </div>
-            </div>
-        </div>
-        <div className="goal-content-container">
-            <div className="goal-content">
-                <div className="goal-sub-heading">
-                    <h2>Notable Alumni</h2>
-                </div>
-                <div className="article-text">
-                    <p>
-                        Being a GCU alumnus gives you access to an encyclopedia of knowledge by reaching out to 
-                        other alumni and building networks. Share experiences, gain different perspectives on common 
-                        problems, and seek solutions that can benefit the continent. 
-                    </p>
-                    <ul>
-                        <li>Lifetime access to the GCU library is </li> 
-                        <li>Receive the regular news</li>
-                        <li>Get social.</li>
-                        <li>Be a guest lecturer and share your experience.</li>
-                        <li>Attend courses and lectures to continue your path of lifelong learning.</li>
-                        <li>Fund bursaries or help raise funds.  </li>
-                    </ul>
+                    <AlumniCard 
+                        image="../../assets/Ripunjoy_Bordoloi.jpg" 
+                        name="Dr. Ripunjoy Bordoloi" 
+                        batch="Batch: 2009-2015" 
+                        curr_pos="Current Position: Inspector Of Drugs" 
+                        curr_emp="Current Employer: Govt. Of Assam"
+                        message="Message: Dear Girijananda Chowdhury University and the Department of Pharmaceutics,As an alumnus, I am proud to witness the remarkable growth of our university and department. The foundation I received here has been instrumental in shaping my career. Wishing continued success to the students and faculty in advancing excellence in pharmaceutical education and research." 
+                    />
+                    
                 </div>
             </div>
         </div>      
